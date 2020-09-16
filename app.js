6fc75d9b37b6e17d69fc95f793670e981de0bdd3
the-cross-art <imrannaz326@gmail.com>
2020-09-16 17:05:52 +0530

    node start

--- app.js
+++ app.js
@@ -68,4 +68,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.listen(3000, function() { 
+  console.log('Server listening on port 3000'); 
+});
+
 module.exports = app;
